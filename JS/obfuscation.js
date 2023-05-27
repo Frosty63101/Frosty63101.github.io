ffab7a4446e8f55473bd32783132d2e4c53c2162
Frosty63101 <samfrostman@icloud.com>
2023-05-26 20:23:24 -0400

    complication

--- JS/obfuscation.js
+++ JS/obfuscation.js
@@ -1,6 +1,5 @@
 function unscrambleKey(obfuscatedKeyOne, obfuscatedKeyTwo, obfucatedKeyThree, obfucatedKeyFour) {
     var obfuscatedKey = obfuscatedKeyOne + obfucatedKeyThree + obfuscatedKeyTwo + obfucatedKeyFour;
-    var deobfuscatedKey = deobfuscateString(obfuscatedKey);
     return deobfuscatedKey;
 }
 
@@ -8,19 +7,3 @@ function wholeKey(obfuscatedKeyOne, obfuscatedKeyTwo, obfucatedKeyThree, obfucat
     var obfuscatedKey = obfuscatedKeyOne + obfucatedKeyThree + obfuscatedKeyTwo + obfucatedKeyFour;
     return obfuscatedKey;
 }
-
-function deobfuscateString(inputString) {
-    var deobfuscatedStrings = "";
-    for (var i = 0; i < inputString.length;) {
-        var charCode = inputString.charCodeAt(i);
-        charCode += 10; // reverse the character code modification
-        deobfuscatedStrings += String.fromCharCode(charCode);
-        i++;
-        var charCode = inputString.charCodeAt(i);
-        charCode -= 10; // reverse the character code modification
-        deobfuscatedStrings += String.fromCharCode(charCode);
-        i++
-    }
-    console.log();
-    return deobfuscatedStrings;
-  }
