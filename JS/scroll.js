56a9ae93d1c5f31f4f4a2ca88190ef7c88b14883
Frosty63101 <samfrostman@icloud.com>
2023-05-30 23:40:04 -0400

    asddf

--- JS/scroll.js
+++ JS/scroll.js
@@ -12,21 +12,50 @@ function scrollToTop() {
     });
 }
 
-window.addEventListener('scroll', function() {
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+  
+window.addEventListener('scroll', async function() {
     var totopButton = document.getElementById('totop');
     var totopLabel = document.querySelector('.toplabel');
     var elementH = document.getElementById("header");
     var dropAdd = document.getElementById("dropAdd");
     var headMove = document.getElementById("headMove");
     var x = document.getElementById("dropdown");
+    
     if (window.scrollY > 0) {
-        totopButton.style.display = 'block';
-        totopLabel.style.display = 'block';
-        elementH.style.left = '65%';
-        elementH.style.marginRight = '-13%';
-        dropAdd.style.display = "none";
-        headMove.style.display = "none";
+      await delay(10);
+      totopButton.style.display = 'block';
+      totopLabel.style.display = 'block';
+      elementH.style.left = '65%';
+      elementH.style.marginRight = '-13%';
+      dropAdd.style.display = "none";
+      headMove.style.display = "none";
     } else {
+      await delay(10);
+      totopButton.style.display = 'none';
+      totopLabel.style.display = 'none';
+      elementH.style.left = '2%';
+      elementH.style.marginRight = '63%';
+      headMove.style.display = "block";
+      if (x.style.display !== "none") {
+        dropAdd.style.display = "block";
+      }
+    }
+  });
+  
+
+window.addEventListener('DOMContentLoaded', function() {
+    var totopButton = document.getElementById('totop');
+    var totopLabel = document.querySelector('.toplabel');
+    var elementH = document.getElementById("header");
+    var dropAdd = document.getElementById("dropAdd");
+    var headMove = document.getElementById("headMove");
+    var x = document.getElementById("dropdown");
+    if (window.scrollY === 0) {
+        totopButton.style.display = 'none';
+        totopLabel.style.display = 'none';
         totopButton.style.display = 'none';
         totopLabel.style.display = 'none';
         elementH.style.left = '2%';
@@ -36,13 +65,12 @@ window.addEventListener('scroll', function() {
             dropAdd.style.display = "block";
         }
     }
-});
-
-window.addEventListener('DOMContentLoaded', function() {
-    var totopButton = document.getElementById('totop');
-    var totopLabel = document.querySelector('.toplabel');
-    if (window.scrollY === 0) {
-        totopButton.style.display = 'none';
-        totopLabel.style.display = 'none';
+    else {
+      totopButton.style.display = 'block';
+      totopLabel.style.display = 'block';
+      elementH.style.left = '65%';
+      elementH.style.marginRight = '-13%';
+      dropAdd.style.display = "none";
+      headMove.style.display = "none";
     }
 });
