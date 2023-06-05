b41b0a08470b1e04e9d43b03aba21b06760f9947
Frosty63101 <92688985+Frosty63101@users.noreply.github.com>
2023-06-05 12:23:33 -0400

    GAY

--- JS/COLORS.js
+++ JS/COLORS.js
@@ -1,28 +1,38 @@
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const whileToggle = false;
-const toggle = document.getElementById(rainbowToggle);
-
-
-async function COLORS() {
+  }
+  
+  let whileToggle = false;
+  const toggle = document.getElementById("rainbowToggle");
+  
+  toggle.addEventListener('click', clicked);
+  
+  function clicked() {
+    console.log("clicked");
+    whileToggle = !whileToggle; // Toggle the value of whileToggle
+    if (whileToggle) {
+      COLORS(); // Run COLORS if whileToggle is true
+    }
+  }
+  
+  async function COLORS() {
     var body = document.getElementById("body");
-
-do {
-        body.style.color = "#ff0000"
-        await delay(10);
-        body.style.color = "#ffa500"
-        await delay(10);
-        body.style.color = "#ffff00"
-        await delay(10);
-        body.style.color = "#008000"
-        await delay(10);
-        body.style.color = "#0000ff"
-        await delay(10);
-        body.style.color = "#4b0082"
-        await delay(10);
-        body.style.color = "#ee82ee"
-        await delay(10);
-} while (whileToggle === true);
-}
+  
+    do {
+      body.style.color = "#ff0000";
+      await delay(100);
+      body.style.color = "#ffa500";
+      await delay(100);
+      body.style.color = "#ffff00";
+      await delay(100);
+      body.style.color = "#008000";
+      await delay(100);
+      body.style.color = "#0000ff";
+      await delay(100);
+      body.style.color = "#4b0082";
+      await delay(100);
+      body.style.color = "#ee82ee";
+      await delay(100);
+    } while (whileToggle);
+  }
+  
