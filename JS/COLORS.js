a13506bf8ade84f2eaee0b0b6f3c2456df1b9c33
Frosty63101 <92688985+Frosty63101@users.noreply.github.com>
2023-06-06 12:48:12 -0400

    pride with toggle fixes

--- JS/COLORS.js
+++ JS/COLORS.js
@@ -1,39 +1,44 @@
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let whileToggle = false;
+const toggle = document.getElementById("rainbowToggle");
+
+toggle.addEventListener('click', clicked);
+
+function clicked() {
+  console.log("clicked");
+  var body = document.getElementById("body");
+  
+  whileToggle = !whileToggle;
+  if (whileToggle) {
+    COLORS(body);
+  } else {
+    body.style.color = "";
   }
+}
+
+async function COLORS(body) {
+  var originalColor = body.style.color;
   
-  let whileToggle = false;
-  const toggle = document.getElementById("rainbowToggle");
+  do {
+    body.style.color = "#ff0000";
+    await delay(100);
+    body.style.color = "#ffa500";
+    await delay(100);
+    body.style.color = "#ffff00";
+    await delay(100);
+    body.style.color = "#008000";
+    await delay(100);
+    body.style.color = "#0000ff";
+    await delay(100);
+    body.style.color = "#4b0082";
+    await delay(100);
+    body.style.color = "#ee82ee";
+    await delay(100);
+    body.style.color = "#e3dfff";
+  } while (whileToggle);
   
-  toggle.addEventListener('click', clicked);
-  
-  function clicked() {
-    console.log("clicked");
-    whileToggle = !whileToggle; // Toggle the value of whileToggle
-    if (whileToggle) {
-      COLORS(); // Run COLORS if whileToggle is true
-    }
-  }
-  
-  async function COLORS() {
-    var body = document.getElementById("body");
-  
-    do {
-      body.style.color = "#ff0000";
-      await delay(100);
-      body.style.color = "#ffa500";
-      await delay(100);
-      body.style.color = "#ffff00";
-      await delay(100);
-      body.style.color = "#008000";
-      await delay(100);
-      body.style.color = "#0000ff";
-      await delay(100);
-      body.style.color = "#4b0082";
-      await delay(100);
-      body.style.color = "#ee82ee";
-      await delay(100);
-      body.style.color = "#e3dfff";
-    } while (whileToggle);
-  }
-  
+  body.style.color = originalColor;
+}
